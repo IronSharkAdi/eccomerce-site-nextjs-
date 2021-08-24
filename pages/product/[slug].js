851dae3ff120087a6cce4a617d02c3980856a5b1
Iron Shark Adi <ironsharkadi2020@gmail.com>
2021-08-24 21:26:20 +0600

    temp

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,8 +1,12 @@
-import { Typography } from "@material-ui/core";
+import { CardMedia, Typography } from "@material-ui/core";
 import { useRouter } from "next/router";
 import data from "../../utils/data";
+import Layout from "../../component/Layout";
+import { Link } from '@material-ui/core'
+import NextLink from 'next/link'
 
-function slug() {
+
+function slug({imgAlt , imgPic}) {
   const router = useRouter() ;
   const { slug } = router.query;
   const product = data.products.find((x) => x.slug === slug);
@@ -13,7 +17,13 @@ function slug() {
 
   return (
     <>
-      <Typography>{product.name}</Typography>
+      <Layout>
+        <div>
+          <NextLink>
+            <Link>Back</Link>
+          </NextLink>
+        </div>
+      </Layout>
     </> 
 
   );
